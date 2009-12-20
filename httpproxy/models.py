bad236572a793a54707f83d6ff8467d3e8f45dd1
Yuri van der Meer <info@yvandermeer.net>
2009-12-21 00:44:37 +0100

    Backed out changeset 4e40aec90e5a to have another attempt at storing each Request Parameter for a Request separately.

--- httpproxy/models.py
+++ httpproxy/models.py
@@ -12,11 +12,15 @@ class Request(models.Model):
     date = models.DateTimeField(auto_now=True)
     querystring = models.CharField(_('querystring'), max_length=250, blank=True)
     
+    # @property
+    # def querystring(self):
+    #     return self.parameters.urlencode()
+
     def __unicode__(self):
         output = u'%s:%d%s' % (self.domain, self.port, self.path)
         if self.querystring:
             output += '?%s' % self.querystring
-        return output[:50] # TODO add elipsis if truncating
+        return output[:50] # TODO add elipsed if truncating
 
     class Meta:
         verbose_name = _('request')
@@ -24,6 +28,34 @@ class Request(models.Model):
         unique_together = ('domain', 'port', 'path', 'querystring')
         get_latest_by = 'date'
 
+
+# class RequestParameterManager(models.Manager):
+#     
+#     def urlencode(self):
+#         output = []
+#         for param in self.values('name', 'value'):
+#             output.extend([urlencode({param['name']: param['value']})])
+#         return '&'.join(output)
+    
+
+# class RequestParameter(models.Model):
+#     REQUEST_TYPES = (
+#         ('G', 'GET'),
+#         ('P', 'POST'),
+#     )
+#     request = models.ForeignKey(Request, verbose_name=_('request'), related_name='parameters')
+#     type = models.CharField(max_length=1, choices=REQUEST_TYPES, default='G')
+#     name = models.CharField(_('name'), max_length=20)
+#     value = models.CharField(_('value'), max_length=200, null=True, blank=True)
+#     objects = RequestParameterManager()
+# 
+#     def __unicode__(self):
+#         return u'%d %s=%s' % (self.pk, self.name, self.value)
+# 
+#     class Meta:
+#         verbose_name = _('request parameter')
+#         verbose_name_plural = _('request parameters')
+    
 
 class Response(models.Model):
     request = models.OneToOneField(Request, verbose_name=_('request'))

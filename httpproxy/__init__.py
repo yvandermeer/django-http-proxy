604ac6a3a71b7735690d8bfcfcb62d8ac0a22faf
Yuri van der Meer <info@yvandermeer.net>
2009-12-23 11:29:19 +0100

    Upped to v0.3.2.

--- httpproxy/__init__.py
+++ httpproxy/__init__.py
@@ -2,5 +2,5 @@
 Django HTTP Proxy - A simple HTTP proxy for the Django framework.
 """
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __authors__ = ['Yuri van der Meer <django-http-proxy@yvandermeer.net>']

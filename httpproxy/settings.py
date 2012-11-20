f61d586ef0ed90485d9b7871b0724ebc81e2f111
Yuri van der Meer <info@yvandermeer.net>
2012-11-20 09:46:36 +0100

    Cleaned up old settings

--- httpproxy/settings.py
+++ httpproxy/settings.py
@@ -6,20 +6,12 @@ Provides defaults and sensible error messages for settings used by the
 'httpproxy' application.
 """
 
-try:
-    PROXY_DOMAIN = getattr(settings, 'PROXY_DOMAIN')
-except AttributeError:
-    from django.core.exceptions import ImproperlyConfigured
-    raise ImproperlyConfigured("To use the 'httpproxy' app, please add the PROXY_DOMAIN setting to your settings module.")
-
 PROXY_PORT = getattr(settings, 'PROXY_PORT', 80)
 
 # Optional authentication
 if hasattr(settings, 'PROXY_USER') and hasattr(settings, 'PROXY_PASSWORD'):
     PROXY_USER = settings.PROXY_USER
     PROXY_PASSWORD = settings.PROXY_PASSWORD
-
-PROXY_MODE = getattr(settings, 'PROXY_MODE', None)
 
 PROXY_IGNORE_UNSUPPORTED = getattr(settings, 'PROXY_IGNORE_UNSUPPORTED', True)
 

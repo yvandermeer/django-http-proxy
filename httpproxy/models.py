94f26a2ca9f5c6157aa2684cf166ebd2462ef6f2
spectralsun <garrett@spectralsun.com>
2014-01-07 21:04:24 -0800

    Patched depecrecated truncate_words

--- httpproxy/models.py
+++ httpproxy/models.py
@@ -1,7 +1,14 @@
 from urllib import urlencode
 
 from django.db import models
-from django.utils.text import truncate_words
+try:
+    from django.utils.text import Truncator
+except ImportError:
+    from django.utils.text import truncate_words # Django < 1.4
+else:
+    def truncate_words(s, num, end_text='...'):
+        end_text = ' {0}'.format(end_text) if end_text else ''
+        return Truncator(s).words(num, truncate=end_text)
 from django.utils.translation import ugettext as _
 
 

450adbb24448b6fb96c589fcea1d875c39aa2ec9
Yuri van der Meer <yuri@yvandermeer.net>
2015-02-09 05:52:15 +0100

    Use versioneer for versioning

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
 from setuptools import setup, find_packages
+import versioneer
 
-#import os.path
-#import distutils
-#from distutils.core import setup
 
-from httpproxy import __version__
+packages = find_packages()
 
-#here = os.path.dirname(os.path.abspath(__file__))
-
-# Get long_description from index.txt
-#f = open(os.path.join(here, 'docs', 'index.txt'))
-#long_description = f.read().strip()
-#long_description = long_description.split('split here', 1)[1]
-#f.close()
+versioneer.VCS = 'git'
+versioneer.versionfile_source = '{0}/_version.py'.format(packages[0])
+versioneer.tag_prefix = ''
+versioneer.parentdir_prefix = ''
 
 setup(
     name='django-http-proxy',
-    version=__version__,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description='A simple HTTP proxy for the Django framework.',
-    #long_description=long_description,
     author='Yuri van der Meer',
     author_email='django-http-proxy@yvandermeer.net',
-    url='http://httpproxy.yvandermeer.net/',
-    download_url='http://bitbucket.org/yvandermeer/django-http-proxy/get/v%s.zip' % __version__,
+    url='http://django-http-proxy.readthedocs.org/',
     packages=find_packages(),
+    install_requires=[
+        'Django>=1.1',
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',

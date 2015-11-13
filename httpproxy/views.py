550cb5e451a9ba1b1aeaf3b9e53dd13f33c6b87b
Ihor Kucher <igor.k.843@gmail.com>
2015-11-13 14:20:09 +0200

    add support of post requests

--- httpproxy/views.py
+++ httpproxy/views.py
@@ -137,20 +137,12 @@ class HttpProxy(View):
         url = urllib.parse(self.base_url)
         return ProxyRecorder(domain=url.hostname, port=(url.port or 80))
 
-    def get(self, request, *args, **kwargs):
-        request_url = self.get_full_url(self.url)
-        request = self.create_request(request_url)
-        response = urllib.request.urlopen(request)
-        try:
-            response_body = response.read()
-            status = response.getcode()
-            logger.debug(self._msg % response_body)
-        except urllib.error.HTTPError as e:
-            response_body = e.read()
-            logger.error(self._msg % response_body)
-            status = e.code
-        return HttpResponse(response_body, status=status,
-                content_type=response.headers['content-type'])
+    def get(self, *args, **kwargs):
+        return self.get_response()
+
+    def post(self, request, *args, **kwargs):
+        headers = {'Content-type': request.META.get('CONTENT_TYPE')}
+        return self.get_response(body=request.body, headers=headers)
 
     def get_full_url(self, url):
         """
@@ -165,3 +157,18 @@ class HttpProxy(View):
         request = urllib.request.Request(url, body, headers)
         logger.info('%s %s' % (request.get_method(), request.get_full_url()))
         return request
+
+    def get_response(self, body=None, headers={}):
+        request_url = self.get_full_url(self.url)
+        request = self.create_request(request_url, body=body, headers=headers)
+        response = urllib.request.urlopen(request)
+        try:
+            response_body = response.read()
+            status = response.getcode()
+            logger.debug(self._msg % response_body)
+        except urllib.error.HTTPError as e:
+            response_body = e.read()
+            logger.error(self._msg % response_body)
+            status = e.code
+        return HttpResponse(response_body, status=status,
+                            content_type=response.headers['content-type'])
